88b3a8a071ab17fc9071df1b507a8f741f969a34
Nagaraj Hebbar <nagaraj.hebbar12@gmail.com>
2025-01-24 23:05:41 +0530

    Dockerized application

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -79,7 +79,6 @@ const SignUp = () => {
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin
-            clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
             render={(renderProps) => (
               <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb://localhost:27017/Democritic';
+const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
